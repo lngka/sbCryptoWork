20686195a49552ea771b9f41317f3cafbc159232
lngka <foxlead@gmail.com>
2018-03-12 20:59:43 +0100

    indicators planned

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+function httpGetAsync(theUrl, callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous
+    xmlHttp.send();
+}
+
+httpGetAsync("http://localhost:3000/p?from=BTC&to=USD", function (responseText) {
+    console.log("Server say: " + responseText);
+})
